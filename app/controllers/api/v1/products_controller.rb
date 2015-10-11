628bc701319b64e454499332b16e4911ade2d07f
Kasperi Keski-Loppi <kasperi.keski.loppi@gmail.com>
2015-10-11 19:27:49 +0300

    Add #index to products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::ProductsController < ApplicationController
+
+  def index
+    render json: { :products => Product.all }
+  end
+
   def show
     render json: Product.find(params[:id])
   end
+
 end
--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -14,4 +14,18 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
 
     it { should respond_with 200 }
   end
+
+  describe "GET #index" do
+    before(:each) do
+      4.times { FactoryGirl.create :product }
+      get :index
+    end
+
+    it "returns 4 records from the database" do
+      products_response = json_response
+      expect(products_response[:products].size).to eq(4)
+    end
+
+    it { should respond_with 200 }
+  end
 end
