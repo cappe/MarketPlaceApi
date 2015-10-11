d40e9706a68f6760800542c2dad34b07c849be6e
Kasperi Keski-Loppi <kasperi.keski.loppi@gmail.com>
2015-10-11 19:43:43 +0300

    Add #create to products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::ProductsController < ApplicationController
+  before_action :authenticate_with_token!, only: [:create]
 
   def index
     render json: { :products => Product.all }
@@ -7,5 +8,20 @@ class Api::V1::ProductsController < ApplicationController
   def show
     render json: Product.find(params[:id])
   end
+
+  def create
+    product = current_user.products.build(product_param)
+    if product.save
+      render json: product, status: 201, location: [:api, product]
+    else
+      render json: { errors: product.errors }, status: 422
+    end
+  end
+
+  private
+
+    def product_param
+      params.require(:product).permit(:title, :price, :published)
+    end
 
 end
