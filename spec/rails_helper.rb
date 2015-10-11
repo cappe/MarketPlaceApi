893eda5a7ef368496dd08ef29d039ba5371d1bc3
Kasperi Keski-Loppi <kasperi.keski.loppi@gmail.com>
2015-10-11 11:21:16 +0300

    Add sesions controller create action

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -58,8 +58,10 @@ RSpec.configure do |config|
     end
   end
 
+  # Including to test requests
   config.include Request::JsonHelpers, :type => :controller
   config.include Request::HeadersHelpers, :type => :controller
+  config.include Devise::TestHelpers, :type => :controller
 
   config.before(:each, type: :controller) do
     include_default_accept_headers
