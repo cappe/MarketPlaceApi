fd706ec43d877d1ffd40dba6b28e6b0d13a01715
Kasperi Keski-Loppi <kasperi.keski.loppi@gmail.com>
2015-10-11 12:41:08 +0300

    Fix RSpec bug with this code

--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-class Authentication
+class Authentication < ActionController::Base
   include Authenticable
 end
 
@@ -11,12 +11,11 @@ RSpec.describe Authenticable do
   describe "#current_user" do
     before do
       @user = FactoryGirl.create :user
-      request.headers['Authorization'] = @user.auth_token
-      authentication.stub(:request).and_return(request)
+      request.headers["Authorization"] = @user.auth_token
+      allow(authentication).to receive(:request).and_return(request)
     end
-
-    # it "returns the user from the authorization header" do
-    #   expect(authentication.current_user.auth_token).to eql @user.auth_token
-    # end
+    it "returns the user from the authorization header" do
+      expect(authentication.current_user.auth_token).to eql @user.auth_token
+    end
   end
 end
