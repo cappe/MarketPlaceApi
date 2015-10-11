5dde9c2a58e9a4452131daf75a68cfa3aaf30f1a
Kasperi Keski-Loppi <kasperi.keski.loppi@gmail.com>
2015-10-11 20:36:35 +0300

    Add #update to products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::ProductsController < ApplicationController
-  before_action :authenticate_with_token!, only: [:create]
+  before_action :authenticate_with_token!, only: [:create, :update]
 
   def index
-    render json: { :products => Product.all }
+    render json: { products: Product.all }
   end
 
   def show
@@ -10,7 +10,7 @@ class Api::V1::ProductsController < ApplicationController
   end
 
   def create
-    product = current_user.products.build(product_param)
+    product = current_user.products.build(product_params)
     if product.save
       render json: product, status: 201, location: [:api, product]
     else
@@ -18,9 +18,18 @@ class Api::V1::ProductsController < ApplicationController
     end
   end
 
+  def update
+    product = current_user.products.find(params[:id])
+    if product.update(product_params)
+      render json: product, status: 200, location: [:api, product]
+    else
+      render json: { errors: product.errors }, status: 422
+    end
+  end
+
   private
 
-    def product_param
+    def product_params
       params.require(:product).permit(:title, :price, :published)
     end
 
