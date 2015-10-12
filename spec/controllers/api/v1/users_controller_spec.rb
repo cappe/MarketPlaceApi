302641a2120d837e24214c4f2c2020b3417824bd
Kasperi Keski-Loppi <kasperi.keski.loppi@gmail.com>
2015-10-12 11:53:31 +0300

    Add test to check that the user has products as an embeded object

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -14,6 +14,11 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     end
 
     it { should respond_with 200 }
+
+    it "has the products as an embeded object" do
+      user_response = json_response[:user]
+      expect(user_response[:products]).to eql []
+    end
   end
 
   describe "POST #create" do
