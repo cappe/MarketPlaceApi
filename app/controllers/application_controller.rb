e5ca77174dcb2bb2f7dc7db72a4e2c2da42e4c17
Kasperi Keski-Loppi <kasperi.keski.loppi@gmail.com>
2015-10-13 19:27:40 +0300

    Remove CSRF and inherit from API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
-
+class ApplicationController < ActionController::API
   include Authenticable
   include ActionController::Serialization
 
