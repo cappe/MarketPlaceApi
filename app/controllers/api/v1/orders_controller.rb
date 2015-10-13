d766d900ac915f3c329c1c3385041a17ac1d9fc8
Kasperi Keski-Loppi <kasperi.keski.loppi@gmail.com>
2015-10-13 14:30:23 +0300

    Allow the order to be placed along with product quantity

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -10,10 +10,11 @@ class Api::V1::OrdersController < ApplicationController
   end
 
   def create
-    logger.debug params
-    order = current_user.orders.build(order_params)
+    order = current_user.orders.build
+    order.build_placement_with_product_ids_and_quantities(params[:order][:product_ids_and_quantities])
 
     if order.save
+      order.reload
       render json: order, status: 201, location: [:api, current_user, order]
     else
       render json: { errors: current_user.errors}, status: 422
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151012121704) do
+ActiveRecord::Schema.define(version: 20151013112411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,8 +28,9 @@ ActiveRecord::Schema.define(version: 20151012121704) do
   create_table "placements", force: :cascade do |t|
     t.integer  "order_id"
     t.integer  "product_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "quantity",   default: 0
   end
 
   add_index "placements", ["order_id"], name: "index_placements_on_order_id", using: :btree
@@ -42,6 +43,7 @@ ActiveRecord::Schema.define(version: 20151012121704) do
     t.integer  "user_id"
     t.datetime "created_at",                                         null: false
     t.datetime "updated_at",                                         null: false
+    t.integer  "quantity",                           default: 0
   end
 
   add_index "products", ["user_id"], name: "index_products_on_user_id", using: :btree
